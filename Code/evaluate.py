7a7dbf205063f4d736db5f04e97e3be8c25973ae
riccardosimionato <ScienzStudio>
2022-01-20 12:10:25 +0100

    Dense

--- Code/evaluate.py
+++ Code/evaluate.py
@@ -6,33 +6,42 @@ import os
 import glob
 import matplotlib.pyplot as plt
 
-data_dir = 'C:/Users/riccarsi/Documents/GitHub/Results/DenseFeed_Testing/WavPredictions'
-data_dir = 'C:/Users/riccarsi/Documents/GitHub/Results/LSTM2_Testing/WavPredictions'
+#data_dir_dense = 'C:/Users/riccarsi/Documents/GitHub/Results/DenseFeed_Testing/WavPredictions'
+#data_dir_dense = 'C:/Users/riccarsi/Documents/GitHub/Results/DenseFeed_Testing/WavPredictions'
+#data_dir_LSTM = 'C:/Users/riccarsi/Documents/GitHub/Results/LSTM2_Testing/WavPredictions'
+#data_dir_LSTM = 'C:/Users/riccarsi/Documents/GitHub/Results/LSTM2_Testing/WavPredictions'
 
-file_tar = glob.glob(os.path.normpath('/'.join([data_dir, '_tar.wav'])))
-file_pred = glob.glob(os.path.normpath('/'.join([data_dir, '_pred.wav'])))
+file_tar_dense = glob.glob(os.path.normpath('/'.join([data_dir_dense, '_tar.wav'])))
+file_tar_LSTM = glob.glob(os.path.normpath('/'.join([data_dir_LSTM, '_tar.wav'])))
+file_pred_dense = glob.glob(os.path.normpath('/'.join([data_dir_dense, '_pred.wav'])))
+file_pred_LSTM = glob.glob(os.path.normpath('/'.join([data_dir_LSTM, '_pred.wav'])))
 
-for file in file_tar:
-    fs, audio_tar = wavfile.read(file)
-for file in file_pred:
-    _, audio_pred = wavfile.read(file)
+for file in file_tar_dense:
+    fs, audio_tar_dense = wavfile.read(file)
+for file in file_pred_dense:
+    _, audio_pred_dense = wavfile.read(file)
+for file in file_tar_LSTM:
+    fs, audio_tar_LSTM = wavfile.read(file)
+for file in file_pred_LSTM:
+    _, audio_pred_LSTM = wavfile.read(file)
 
-audio_tar = audio_tar.astype(np.float32)
-audio_pred = audio_pred.astype(np.float32)
-audio_tar = audio_tar[:len(audio_pred)]
+#Dense
+audio_tar_dense = audio_tar_dense.astype(np.float32)
+audio_pred_dense = audio_pred_dense.astype(np.float32)
+audio_tar_dense = audio_tar_dense[:len(audio_pred_dense)]
 
-time = np.linspace(0, len(audio_tar) / fs, num=len(audio_tar))
-N = len(audio_tar)
+time = np.linspace(0, len(audio_tar_dense) / fs, num=len(audio_tar_dense))
+N = len(audio_tar_dense)
 fs = 1600
-fft_tar = fft.fftshift(fft.fft(audio_tar))[N//2:]
-fft_pred = fft.fftshift(fft.fft(audio_pred))[N//2:]
+fft_tar = fft.fftshift(fft.fft(audio_tar_dense))[N//2:]
+fft_pred = fft.fftshift(fft.fft(audio_pred_dense))[N//2:]
 freqs = fft.fftshift(fft.fftfreq(N)*fs)
 freqs = freqs[N//2:]
 
 fig, ax = plt.subplots()
 plt.title("Target vs Predictionn - Time Domain")
-ax.plot(time, audio_tar, label='Target')
-ax.plot(time, audio_pred, label='Prediction')
+ax.plot(time, audio_tar_dense, label='Target')
+ax.plot(time, audio_pred_dense, label='Prediction')
 ax.set_xlabel('Time')
 ax.set_ylabel('Amplitude')
 ax.legend()
